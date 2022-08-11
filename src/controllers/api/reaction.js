e9693d62d4a22178db663236a6b3833dfba59570
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-08-11 17:17:45 +0100

    potsmanCollection

--- src/controllers/api/reaction.js
+++ src/controllers/api/reaction.js
@@ -1,5 +1,23 @@
-const createAReaction = (req, res) => {
-  res.send("creatAReaction");
+const connection = require("../../config/connection");
+
+const Reaction = require("../../models/Reaction");
+
+const server = require("../../index");
+
+const mongoose = require("mongoose");
+
+const createAReaction = async (req, res) => {
+  try {
+    connection();
+    // get a reaction
+    const { id } = req.params;
+    const { reactionBody, userName } = req.body;
+    // findandUpdate reaction
+
+    // push updated reaction into reaction
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const deleteAReaction = (req, res) => {
